df82bcfb15f417766cf7b53059452e12dcabe5cf
Hanh Solo <minhhanhnyc@gmail.com>
2021-02-10 22:05:39 -0800

    index and temperature routes

--- weather.py
+++ weather.py
@@ -4,5 +4,20 @@ from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 load_dotenv()
-api_key = os.getenv('API_KEY')
-base_url = 'https://api.openweathermap.org/data/2.5/'
+str_api_key = os.getenv('API_KEY')
+str_base_url = 'https://api.openweathermap.org/data/2.5/'
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/temperature', methods=['GET'])
+@cross_origin()
+def temperature():
+  str_current_city = requests.args.get('Current city')
+  r = requests.get(str_base_url + 'weather?q='+str_current_city+'&units=imperial&appid='+str_api_key+'')
+  return r.json()
+  
